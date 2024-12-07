04fcf9102bdb4bc23addc347e7aa0a5f2c1a9e3f
Korolev E <nuuchcha@bk.ru>
2024-12-07 11:53:13 +0500

    Add function get_date in widget.py

--- src/widget.py
+++ src/widget.py
@@ -12,3 +12,9 @@ def mask_account_card(user_card: str) -> str:
         number_kard = user_card[-16:]
         user_card_mask = user_card[:-16] + get_mask_card_number(number_kard)
     return user_card_mask
+
+
+def get_date(date_not_formatted: str) -> str:
+    """Функция возвращает дату в читаемом виде ДД.ММ.ГГГГ"""
+
+    return date_not_formatted[8:10] + '.' + date_not_formatted[5:7] + '.' + date_not_formatted[:4]
