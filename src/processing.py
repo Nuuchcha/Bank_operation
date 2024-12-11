6217668ffdb5c73ed222c79a68d339b50a5aa28c
Korolev E <nuuchcha@bk.ru>
2024-12-11 19:58:31 +0500

    Add processing.py

---
+++ src/processing.py
@@ -0,0 +1,15 @@
+def filter_by_state(list_dict: list, state: list='EXECUTED') -> list:
+    """ Функция выводит список словарей, у которых ключ 'state' соответствует заданному значению """
+
+    new_list_dict = []
+    for item_list in list_dict:
+        if item_list['state'] == state:
+            new_list_dict.append(item_list)
+    return new_list_dict
+
+
+def sort_by_date(list_dict: list, sort: bool=True) -> list:
+    """ Функция возвращвет список отсортированный по дате """
+
+    sorted_list_dict = sorted(list_dict, key=lambda i: i['date'], reverse=sort)
+    return sorted_list_dict
