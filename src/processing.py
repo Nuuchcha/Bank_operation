b881f18b41610ea74c8f8f37c7e14e1fb04a1624
Korolev E <nuuchcha@bk.ru>
2024-12-12 19:44:35 +0500

    Fix type annotations and docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,11 @@
-def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
-    """Функция выводит список словарей, у которых ключ 'state' соответствует заданному значению"""
+from typing import Any, Dict, List
+
+
+def filter_by_state(list_dict: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """Функция принимает список словарей и опционально значение для ключа 'state' (по умолчанию 'EXECUTED'),
+    и возвращает новый список словарей, содержащий только те словари, у которых ключ 'state' соответствует
+    указанному значению
+    """
 
     new_list_dict = []
     for item_list in list_dict:
@@ -8,8 +14,10 @@ def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
     return new_list_dict
 
 
-def sort_by_date(list_dict: list, sort: bool = True) -> list:
-    """Функция возвращвет список отсортированный по дате"""
+def sort_by_date(list_dict: List[Dict[str, Any]], sort: bool = True) -> List[Dict[str, Any]]:
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание) и возвращает новый список, отсортированный по дате
+    """
 
     sorted_list_dict = sorted(list_dict, key=lambda i: i["date"], reverse=sort)
     return sorted_list_dict
