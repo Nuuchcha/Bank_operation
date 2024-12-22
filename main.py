ed3b8a9725c8ce9ef7ca2ad9c371a1998c57579d
Korolev E <nuuchcha@bk.ru>
2024-12-22 21:01:43 +0500

    Change README and main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-user_cart_number = input("Введите номер карты без пробелов: ")
-print(get_mask_card_number(user_cart_number))
+user_cart_number = input("Введите название и номер карты через пробел: ")
+print(mask_account_card(user_cart_number))
 
-user_account_number = input("Введите номер счета: ")
-print(get_mask_account(user_account_number))
+user_account_number = input("Введите слово 'Счет' и номер счета через пробел: ")
+print(mask_account_card(user_account_number))
